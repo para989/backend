71f0ffbd914d1a3be2026c2d3cc8d7a56b7805f5
multiflash <mail@multiflash.name>
2021-07-05 13:18:17 +0300

    1.0.14

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ exports.start = async (options) => {
     }
     global.DOMAIN = domain;
     global.MONGODB = _.get(options, 'mongodb', `mongodb://localhost/${_.kebabCase(domain)}`);
-    global.REDIS = _.get(options, 'redis', {
-        host: 'localhost',
-        port: 6379
-    });
+    global.REDIS = _.get(options, 'redis', 'redis://localhost:6379/0');
     global.STORAGE = _.get(options, 'storage');
     global.SITE = _.get(options, 'site', false);
     global.LANG = _.get(options, 'lang', 'en');
