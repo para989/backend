632d593b6bfb7bbce0285309402ae36b15a8c0c5
multiflash <mail@multiflash.name>
2021-07-05 13:42:43 +0300

    Bugs fixed.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ exports.start = async (options) => {
         process.exit(1);
     }
     global.DOMAIN = domain;
-    global.MONGODB = _.get(options, 'mongodb', `mongodb://localhost/${_.kebabCase(domain)}`);
+    global.MONGODB = _.get(options, 'mongodb', `mongodb://localhost:27017/${_.kebabCase(domain)}`);
     global.REDIS = _.get(options, 'redis', 'redis://localhost:6379/0');
     global.STORAGE = _.get(options, 'storage');
     global.SITE = _.get(options, 'site', false);
